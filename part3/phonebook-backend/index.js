b88133688d48a233af014e05a354b261ca944cc9
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-04 15:57:54 +1200

    3.8

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -3,7 +3,15 @@ const app = express();
 const morgan = require("morgan");
 
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token("body", function (request, response) {
+  if (request.method === "POST") {
+    return JSON.stringify(request.body);
+  }
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
