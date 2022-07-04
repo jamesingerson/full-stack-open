4139e3a76efa9f1e501021cc48458dc7e65ef35e
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-04 15:07:02 +1200

    3.5 and 3.6

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -26,6 +26,10 @@ let persons = [
   },
 ];
 
+const generateId = () => {
+  return Math.floor(Math.random() * 100_000_000 + 1);
+};
+
 app.get("/info", (request, response) => {
   response.send(
     `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
@@ -50,6 +54,32 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   persons = persons.filter((p) => p.id !== id);
   response.status(204).end();
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ error: "No name provided" });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ error: "No number provided" });
+  }
+
+  if (persons.find((person) => person.name === body.name) !== undefined) {
+    return response.status(409).json({ error: "Person already in Phonebook" });
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
 });
 
 const unknownEndpoint = (request, response) => {
