1607e728335c00927499b6333ec0252a5281d9c6
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-09 17:56:01 +1200

    clarifying comments and variable names

--- part4/bloglist-backend/utils/list-helper.js
+++ part4/bloglist-backend/utils/list-helper.js
@@ -22,13 +22,17 @@ const mostBlogs = (blogs) => {
   if (blogs.length === 0) {
     return {};
   }
+  // Array of authors names, with repeats
   const authors = blogs.map((blog) => blog.author);
+  // Object with each authors name and count of their posts
   const blogCount = authors.reduce((blogCount, author) => {
     return (blogCount[author] = (blogCount[author] || 0) + 1), blogCount;
   }, {});
-  const topAuthor = Object.keys(blogCount).reduce((a, b) =>
-    blogCount[a] > blogCount[b] ? a : b
+  // Identify the author with the most posts by keeping track of the top author
+  const topAuthor = Object.keys(blogCount).reduce((topAuthor, author) =>
+    blogCount[topAuthor] > blogCount[author] ? topAuthor : author
   );
+  // Return top author object
   return { author: topAuthor, blogs: blogCount[topAuthor] };
 };
 
