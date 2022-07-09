227101f1bc0cdca7d373b392749d81e8edde3642
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-09 13:51:20 +1200

    extraneous console.log

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -101,10 +101,6 @@ app.use(unknownEndpoint);
 
 const errorHandler = (error, request, response, next) => {
   console.error(error.message);
-  console.log("###############################");
-  console.error(error.name, error);
-  console.log("###############################");
-  console.error(error.code);
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "Malformed id" });
