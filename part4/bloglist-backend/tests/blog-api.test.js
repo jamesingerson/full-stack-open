f7f4a6a53f1d9b6f6488f0302f7e561f18526158
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-11 19:16:12 +1200

    4.9

--- part4/bloglist-backend/tests/blog-api.test.js
+++ part4/bloglist-backend/tests/blog-api.test.js
@@ -17,6 +17,12 @@ describe("after initial blogs are saved", () => {
     const response = await api.get("/api/blogs");
     expect(response.body).toHaveLength(helper.initialBlogs.length);
   });
+
+  test("unique property is called id", async () => {
+    const response = await api.get("/api/blogs");
+    const blog = response.body[0];
+    expect(blog.id).toBeDefined;
+  });
 });
 
 afterAll(() => {
