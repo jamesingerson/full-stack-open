18dbe10a7b7d9eb5e650a9d1a3437a936b6973ed
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-12 20:22:56 +1200

    4.19

--- part4/bloglist-backend/controllers/login.js
+++ part4/bloglist-backend/controllers/login.js
@@ -21,7 +21,12 @@ loginRouter.post("/", async (request, response) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  const token = jwt.sign(
+    userForToken,
+    process.env.SECRET,
+    // One Hour
+    { expiresIn: 60 * 60 }
+  );
 
   response
     .status(200)
