4dff7b17adc16639b6f436aac5ea426da97b8bf9
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-15 17:12:21 +1200

    5.9

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -105,9 +105,11 @@ const App = () => {
           <Togglable buttonLabel="new blog" ref={blogFormRef}>
             <BlogForm createBlog={addBlog} />
           </Togglable>
-          {blogs.map((blog) => (
-            <Blog key={blog.id} blog={blog} increaseLikes={increaseLikes} />
-          ))}
+          {blogs
+            .sort((a, b) => b.likes - a.likes)
+            .map((blog) => (
+              <Blog key={blog.id} blog={blog} increaseLikes={increaseLikes} />
+            ))}
         </div>
       )}
     </div>
