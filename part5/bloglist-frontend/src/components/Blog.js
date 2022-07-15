37703f475e761a3f983305bec6ffa9fdc8d64c1d
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-15 16:24:51 +1200

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,44 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const hideWhenExpanded = { display: expanded ? "none" : "" };
+  const showWhenExpanded = {
+    display: expanded ? "" : "none",
+    paddingTop: 2,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const toggleExpanded = () => {
+    setExpanded(!expanded);
+  };
+
+  const increaseLikes = (blog) => {
+    console.log(blog);
+  };
+
+  return (
+    <>
+      <div style={hideWhenExpanded}>
+        {blog.title}{" "}
+        <button onClick={() => toggleExpanded()}>View Details</button>
+      </div>
+      <div style={showWhenExpanded}>
+        <p>{blog.title}</p>
+        <p>{blog.url}</p>
+        <p>
+          {blog.likes}{" "}
+          <button onClick={() => increaseLikes({ blog })}>Like</button>
+        </p>
+        <p>{blog.author}</p>
+        <button onClick={() => toggleExpanded()}>Collapse</button>
+      </div>
+    </>
+  );
+};
+
+export default Blog;
