f8f4354a9e5aebb81d0919435f8c86cafd09923e
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-17 12:24:14 +1200

    5.18 with bonus

--- part5/bloglist-frontend/cypress/e2e/blog-app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog-app.cy.js
@@ -1,6 +1,12 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    const user = {
+      name: "James Ingerson",
+      username: "jamesi",
+      password: "securepassword",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
 
@@ -8,5 +14,28 @@ describe("Blog app", function () {
     cy.contains("username");
     cy.contains("password");
     cy.contains("login");
+  });
+
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("jamesi");
+      cy.get("#password").type("securepassword");
+      cy.get("#login-button").click();
+      cy.contains("James Ingerson logged in");
+    });
+
+    it("fails with wrong credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("jamesi");
+      cy.get("#password").type("incorrect");
+      cy.get("#login-button").click();
+
+      cy.get(".error")
+        .should("contain", "Invalid credentials")
+        .and("have.css", "color", "rgb(255, 0, 0)");
+
+      cy.get("html").should("not.contain", "James Ingerson logged in");
+    });
   });
 });
--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -13,6 +13,7 @@ const LoginForm = ({
         value={username}
         name="Username"
         onChange={({ target }) => setUsername(target.value)}
+        id="username"
       />
     </div>
     <div>
@@ -22,9 +23,12 @@ const LoginForm = ({
         value={password}
         name="Password"
         onChange={({ target }) => setPassword(target.value)}
+        id="password"
       />
     </div>
-    <button type="submit">login</button>
+    <button type="submit" id="login-button">
+      login
+    </button>
   </form>
 );
 
