0ebb79f45e8e2f0b4f277bceed6da4d04fbbc4f0
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-20 11:53:58 +1200

    6.13 and 6.14

--- part6/redux-anecdotes/src/index.js
+++ part6/redux-anecdotes/src/index.js
@@ -7,6 +7,13 @@ import { Provider } from "react-redux";
 
 import store from "./store";
 
+import anecdoteService from "./services/anecdotes";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+
+anecdoteService
+  .getAll()
+  .then((anecdotes) => store.dispatch(setAnecdotes(anecdotes)));
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
