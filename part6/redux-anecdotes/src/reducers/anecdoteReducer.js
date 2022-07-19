0c8c9ad9b3d6d9cf2bcd044cee8bb93c5cb17c99
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-07-19 15:07:07 +1200

    6.10

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from "@reduxjs/toolkit";
+
 const anecdotesAtStart = [
   "If it hurts, do it more often",
   "Adding manpower to a late software project makes it later!",
@@ -6,6 +8,8 @@ const anecdotesAtStart = [
   "Premature optimization is the root of all evil.",
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const getId = () => (100000 * Math.random()).toFixed(0);
 
@@ -19,12 +23,20 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
-const anecdoteReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "NEW_ANECDOTE":
-      return [...state, action.data];
-    case "CAST_VOTE": {
-      const id = action.data.id;
+const anecdoteSlice = createSlice({
+  name: "anecdotes",
+  initialState,
+  reducers: {
+    createAnecdote(state, action) {
+      const content = action.payload;
+      state.push({
+        content,
+        id: generateId(),
+        votes: 0,
+      });
+    },
+    voteFor(state, action) {
+      const id = action.payload;
       const anecdoteToChange = state.find((a) => a.id === id);
       const changedAnecdote = {
         ...anecdoteToChange,
@@ -33,30 +45,9 @@ const anecdoteReducer = (state = initialState, action) => {
       return state.map((anecdote) =>
         anecdote.id !== id ? anecdote : changedAnecdote
       );
-    }
-    default:
-      return state;
-  }
-};
-
-export const voteFor = (id) => {
-  return {
-    type: "CAST_VOTE",
-    data: { id },
-  };
-};
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-export const createAnecdote = (content) => {
-  return {
-    type: "NEW_ANECDOTE",
-    data: {
-      content,
-      id: generateId(),
-      votes: 0,
     },
-  };
-};
+  },
+});
 
-export default anecdoteReducer;
+export const { createAnecdote, voteFor } = anecdoteSlice.actions;
+export default anecdoteSlice.reducer;
