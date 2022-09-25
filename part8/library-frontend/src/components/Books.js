4265a9a4cc96c9b5d39adc18c39efa7ebaef2904
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-09-25 14:20:43 +1300

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,13 +1,34 @@
+import { useState } from "react";
+import "../styles.css";
+
 const Books = (props) => {
+  const [filter, setFilter] = useState("");
+
   if (!props.show) {
     return null;
   }
 
   const books = props.books;
+  const allGenres = new Set(books.map((book) => book.genres).flat());
+
+  const filteredBooks = filter
+    ? books.filter((book) => book.genres.includes(filter))
+    : books;
+  console.log(filteredBooks);
+
+  const filterChange = (filter) => {
+    setFilter(filter);
+  };
 
   return (
     <div>
       <h2>books</h2>
+
+      {!!filter && (
+        <span>
+          Filtered to show: <strong>{filter}</strong>
+        </span>
+      )}
 
       <table>
         <tbody>
@@ -16,15 +37,30 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
+          {filteredBooks.map((book) => (
+            <tr key={book.title}>
+              <td>{book.title}</td>
+              <td>{book.author.name}</td>
+              <td>{book.published}</td>
             </tr>
           ))}
         </tbody>
       </table>
+      {[...allGenres].map((genre) => (
+        <button
+          key={genre}
+          onClick={() => filterChange(genre)}
+          className={genre === filter ? "active" : ""}
+        >
+          {genre}
+        </button>
+      ))}
+      <button
+        onClick={() => filterChange("")}
+        className={!filter ? "active" : ""}
+      >
+        All Genres
+      </button>
     </div>
   );
 };
