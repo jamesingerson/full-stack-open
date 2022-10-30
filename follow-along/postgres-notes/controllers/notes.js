de870b91859e3fa30b70d1f8dd38c0fb0736c974
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-10-30 15:13:46 +1300

    13.17 lead up

--- follow-along/postgres-notes/controllers/notes.js
+++ follow-along/postgres-notes/controllers/notes.js
@@ -1,7 +1,6 @@
-const jwt = require("jsonwebtoken");
-const router = require("express").Router();
+const { tokenExtractor } = require("../util/middleware");
 
-const { SECRET } = require("../util/config");
+const router = require("express").Router();
 
 const { Note, User } = require("../models");
 
@@ -31,20 +30,6 @@ router.get("/", async (req, res) => {
 
   res.json(notes);
 });
-
-const tokenExtractor = (req, res, next) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
-    try {
-      req.decodedToken = jwt.verify(authorization.substring(7), SECRET);
-    } catch {
-      return res.status(401).json({ error: "token invalid" });
-    }
-  } else {
-    return res.status(401).json({ error: "token missing" });
-  }
-  next();
-};
 
 router.post("/", tokenExtractor, async (req, res) => {
   try {
