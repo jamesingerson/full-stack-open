a7880c99ffe6ab0c5ae2d5edf6ca5a60c3b5be3e
James Ingerson <7509500+jamesingerson@users.noreply.github.com>
2022-10-31 19:48:25 +1300

    13.19 lead up

--- follow-along/postgres-notes/controllers/users.js
+++ follow-along/postgres-notes/controllers/users.js
@@ -2,19 +2,21 @@ const { tokenExtractor } = require("../util/middleware");
 
 const router = require("express").Router();
 
-const { User, Note } = require("../models");
-
-// router.get("/", async (req, res) => {
-//   const users = await User.findAll();
-//   res.json(users);
-// });
+const { User, Note, Team } = require("../models");
 
 router.get("/", async (req, res) => {
   const users = await User.findAll({
-    include: {
-      model: Note,
-      attributes: { exclude: ["userId"] },
-    },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: { attributes: [] },
+      },
+    ],
   });
   res.json(users);
 });
@@ -29,13 +31,59 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findByPk(req.params.id);
+  const user = await User.findByPk(req.params.id, {
+    attributes: { exclude: [""] },
+    include: [
+      {
+        model: Note,
+        attributes: { exclude: ["userId"] },
+      },
+      {
+        model: Note,
+        as: "marked_notes",
+        attributes: { exclude: ["userId"] },
+        through: {
+          attributes: [],
+        },
+        include: {
+          model: User,
+          attributes: ["name"],
+        },
+      },
+      {
+        model: Team,
+        attributes: ["name", "id"],
+        through: {
+          attributes: [],
+        },
+      },
+    ],
+  });
+
   if (user) {
     res.json(user);
   } else {
     res.status(404).end();
   }
 });
+
+// router.get("/:id", async (req, res) => {
+//   const user = await User.findByPk(req.params.id, {
+//     include: {
+//       model: Note,
+//     },
+//   });
+
+//   if (user) {
+//     res.json({
+//       username: user.username,
+//       name: user.name,
+//       note_count: user.notes.length,
+//     });
+//   } else {
+//     res.status(404).end();
+//   }
+// });
 
 const isAdmin = async (req, res, next) => {
   const user = await User.findByPk(req.decodedToken.id);
